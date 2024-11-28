66702b69d008b5dcf4099da50c35951f64b327ea
Imran Nazir <106988509+ImranNazir4@users.noreply.github.com>
2024-11-29 00:37:27 +0500

    Update sentiment_analysis_dashboard.py

--- sentiment_analysis_dashboard.py
+++ sentiment_analysis_dashboard.py
@@ -103,11 +103,12 @@ def get_text_metrics(text):
 groq_api_key = os.getenv("GROQ_API_KEY")
 from langchain_groq import ChatGroq
 
-llm = ChatGroq(
-    temperature=0,
-    model="llama-3.1-70b-versatile",
-    api_key=groq_api_key
-)
+llm=()
+
+# llm = ChatGroq(
+#   temperature=0,
+#   model="llama-3.1-70b-versatile",
+#   api_key=groq_api_key)
 
 
 st.title("Caribbean Text Sentiment Analysis System")
