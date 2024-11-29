85ef75b63778e7aa8d76d3d9c234d32288c89201
Imran Nazir <106988509+ImranNazir4@users.noreply.github.com>
2024-11-29 11:53:22 +0500

    Update sentiment_analysis_dashboard.py

--- sentiment_analysis_dashboard.py
+++ sentiment_analysis_dashboard.py
@@ -288,7 +288,7 @@ if st.button("Analyze"):
     # Create a Seaborn bar plot
     sns.set(style="whitegrid")
     fig, ax = plt.subplots(figsize=(7, 5))
-    sns.countplot(emotions_ls,hue=list(set(emotions_ls)),ax=ax)
+    sns.countplot(emotions_ls,ax=ax)
     plt.title("Emotion Analysis")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
     # Display in Streamlit
